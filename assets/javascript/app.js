b1043b4ecfa82c0355696551d98a9981088affe0
Michael DeFrank <mjdefrank@gmail.com>
2017-12-07 17:17:31 -0500

    homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,61 @@
-  		// Initialize Firebase
-  		var config = {
-		    apiKey: "AIzaSyDCJLt3BloP-1jf3MdxJNGZTOuRpB9SFGA",
-		    authDomain: "homework-7-4291d.firebaseapp.com",
+// Initialize Firebase
+	var config = 
+		{
+    		apiKey: "AIzaSyDCJLt3BloP-1jf3MdxJNGZTOuRpB9SFGA",
+    		authDomain: "homework-7-4291d.firebaseapp.com",
 		    databaseURL: "https://homework-7-4291d.firebaseio.com",
 		    projectId: "homework-7-4291d",
-		    storageBucket: "",
+		    storageBucket: "homework-7-4291d.appspot.com",
 		    messagingSenderId: "308635506975"
   		};
-  		firebase.initializeApp(config);
+  	firebase.initializeApp(config);
+	//simplify database calls by creating variable
+	var database = firebase.database();
+	//create onclick function for submit button
+	$(".submitInfo").on('click', function(event) {
+		//prevent default blank submissions
+		event.preventDefault();
+		//set variables for each object property
+		var trainName = $('#trainName').val().trim();
+		var trainDestination = $('#trainDestination').val().trim();
+		var trainTime = $('#trainTime').val().trim(); //need to add momentJS
+		var trainFrequency = $('#trainFrequency').val().trim();
+		
+		var trainInfo = {			
+			name: trainName,
+			destination: trainDestination,
+			time: trainTime,
+			frequency: trainFrequency
+			};
+		// push newTrain into the firebase database
+		database.ref().push(trainInfo);
+		//alert user of successful amendment
+		alert('Train successfully added. Thanks!');
+		//reset field values to blank string
+		$('#trainName').val("");
+		$('#trainDestination').val("");
+		$('#trainTime').val("");
+		$('#trainFrequency').val("");
+	});
 	
-var database = firebase.database();
+	//creates a call to the database for info on the most recent child
+	database.ref().on('child_added', function (snapshot, prevChildKey) {
+		//assign var to snapshot.val() for efficiency
+		var newTrain = snapshot.val();
+		console.log(newTrain);
 
-$(".submitInfo").on('click', function() {
-	var trainName = $('#trainName').val().trim();
-	var trainDestination = $('#trainDestination').val().trim();
-	var trainTime = $('#trainTime').val().trim(); //need to add momentJS
-	var trainFrequency = $('#trainFrequency').val().trim();
-	console.log(trainName, trainDestination, trainTime, trainFrequency);
+		var trainName = newTrain.name;
+		var trainDestination = newTrain.destination;
+		var trainFrequency = moment(newTrain.frequency).format('mm'); //-> in minutes
+		var trainTime = newTrain.time;
+		
+		//set variable for now
+		var now = moment();
+		//prettyify first train time
+		var firstTrain = moment.unix(newTrain.time).format('HH:mm');
+		
+		var minutesAway = 'placeholder'; //I'm not sure how to get this...a for loop maybe? to run through each increment of the trainFrequency?
 
-	var newTrain = {
-		name: trainName,
-		destination: trainDestination,
-		time: trainTime,
-		trainFrequency: trainFrequency
-	};
-
-	database.ref().push(newTrain);
-
-	alert('Train successfully added. Thanks!');
-
-	$('#trainName').val("");
-	$('#trainDestination').val("");
-	$('#trainTime').val("");
-	$('#trainFrequency').val("");
-
-});
+//not appending to table body...I structured this off of the in class example. I'm not sure why it's not working.
+		$('#trainSchedule > tbody').append("<tr><td>"+trainName+"</td><td>"+trainDestination+"</td><td>"+trainFrequency+"</td><td>"+trainTime+"</td><td>"+minutesAway+"</td></tr>");
+	});
